6fe76432e62bf40a66e2a3b9c8cb87df5e3caf7b
LuanVictr <luanvictor2108@gmail.com>
2023-08-15 15:29:11 -0300

    Fix: Corrigindo posicionamento de pastas

--- src/main/java/com/betrybe/agrix/model/entities/Person.java
+++ src/main/java/com/betrybe/agrix/model/entities/Person.java
@@ -1,4 +1,4 @@
-package com.betrybe.agrix.ebytr.staff.entity;
+package com.betrybe.agrix.model.entities;
 
 
 import com.betrybe.agrix.ebytr.staff.security.Role;
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 
 /**
